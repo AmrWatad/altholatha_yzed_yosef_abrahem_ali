ba193cc50c779d5af3dfc4f35e93f67e8f59bbf5
yosefanabosi2011 <yosefanabosi11711@gmail.com>
2026-08-18 19:08:28 +0300

    yosef

--- Yosef.java
+++ Yosef.java
@@ -1,6 +1,6 @@
 public class Yosef {
     public static void main(String[] args) {
         System.out.println("hi");
-        System.out.println("hii");
+        System.out.println("hiiiiiiiiiiiiiiii");
     }
 }
